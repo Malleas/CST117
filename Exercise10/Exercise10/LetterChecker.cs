7cc484a4025c73a4244423561f7bc8ae6baeed16
Matt Sievers <fantsigns@gmail.com>
2019-03-13 18:48:46 -0500

    Initial Commit Exercise 10

---
+++ Exercise10/Exercise10/LetterChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class LetterChecker
+    {
+        public string StringCheker(string input)
+        {
+            int count = 0;
+            string[] stringArray = input.Split(' ');
+       //     string response =
+       //         "There are " + stringArray.Length + " words to check" + "\n" + 
+       //         "The amount of words that end in e are: " + count_E +
+       //         "\n" + "and the amount of words that end in t is: " + count_T;
+            foreach (string word in stringArray)
+            {
+                
+                char last = CheckForPunctuation(word);
+                bool eCheck = last.Equals('e');
+                bool tcheck =last.Equals('t');
+                if(eCheck || tcheck)
+                {
+                    count++;
+                }
+            }
+            string response =
+                "There are " + stringArray.Length + " words to check" + "\n" +
+                "The amount of words that end in e or a t are: " + count ;
+            return response;
+        }
+
+        private char CheckForPunctuation(string word)
+        {
+            
+            char last = word[word.Length - 1];
+            bool isPunctuation = char.IsPunctuation(last);
+            if (isPunctuation)
+            {
+                char newLast = word[word.Length - 2];
+                return newLast;
+            }
+            else return last;
+            
+        }
+ 
+    }
+}
---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LetterChecker checker = new LetterChecker();
+            string input = File.ReadAllText("C:\\Users\\Movie Box\\Dropbox\\GCU Work\\CST-117\\CST117\\Exercise10\\Exercise10\\app_data\\inputFile.txt");
+            string output = checker.StringCheker(input);
+            Console.WriteLine(output);
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+    }
+}
