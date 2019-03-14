bec18803df9a1da742030d829e9095bc4934c09d
Matt Sievers <fantsigns@gmail.com>
2019-03-13 20:49:45 -0500

    initial Commit for PP4

---
+++ Programming Project 4/Programming Project 4/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Programming_Project_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.newGameBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bottomRightLabel = new System.Windows.Forms.Label();
+            this.bottomCenterLabel = new System.Windows.Forms.Label();
+            this.bottomLeftlabel = new System.Windows.Forms.Label();
+            this.centerRightLabel = new System.Windows.Forms.Label();
+            this.centerCenterLabel = new System.Windows.Forms.Label();
+            this.centerLeftLabel = new System.Windows.Forms.Label();
+            this.topRightLabel = new System.Windows.Forms.Label();
+            this.topCenterLabel = new System.Windows.Forms.Label();
+            this.topLeftLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox9 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(144, 221);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(90, 23);
+            this.exitBtn.TabIndex = 41;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // newGameBtn
+            // 
+            this.newGameBtn.Location = new System.Drawing.Point(51, 221);
+            this.newGameBtn.Name = "newGameBtn";
+            this.newGameBtn.Size = new System.Drawing.Size(90, 23);
+            this.newGameBtn.TabIndex = 40;
+            this.newGameBtn.Text = "New Game";
+            this.newGameBtn.UseVisualStyleBackColor = true;
+            this.newGameBtn.Click += new System.EventHandler(this.newGameBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 20);
+            this.textBox1.TabIndex = 39;
+            // 
+            // bottomRightLabel
+            // 
+            this.bottomRightLabel.AutoSize = true;
+            this.bottomRightLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.bottomRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomRightLabel.Location = new System.Drawing.Point(180, 137);
+            this.bottomRightLabel.Name = "bottomRightLabel";
+            this.bottomRightLabel.Size = new System.Drawing.Size(0, 55);
+            this.bottomRightLabel.TabIndex = 38;
+            // 
+            // bottomCenterLabel
+            // 
+            this.bottomCenterLabel.AutoSize = true;
+            this.bottomCenterLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.bottomCenterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomCenterLabel.Location = new System.Drawing.Point(115, 138);
+            this.bottomCenterLabel.Name = "bottomCenterLabel";
+            this.bottomCenterLabel.Size = new System.Drawing.Size(0, 55);
+            this.bottomCenterLabel.TabIndex = 37;
+            // 
+            // bottomLeftlabel
+            // 
+            this.bottomLeftlabel.AutoSize = true;
+            this.bottomLeftlabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.bottomLeftlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomLeftlabel.Location = new System.Drawing.Point(51, 139);
+            this.bottomLeftlabel.Name = "bottomLeftlabel";
+            this.bottomLeftlabel.Size = new System.Drawing.Size(0, 55);
+            this.bottomLeftlabel.TabIndex = 36;
+            // 
+            // centerRightLabel
+            // 
+            this.centerRightLabel.AutoSize = true;
+            this.centerRightLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.centerRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerRightLabel.Location = new System.Drawing.Point(181, 77);
+            this.centerRightLabel.Name = "centerRightLabel";
+            this.centerRightLabel.Size = new System.Drawing.Size(0, 55);
+            this.centerRightLabel.TabIndex = 35;
+            // 
+            // centerCenterLabel
+            // 
+            this.centerCenterLabel.AutoSize = true;
+            this.centerCenterLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.centerCenterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerCenterLabel.Location = new System.Drawing.Point(116, 78);
+            this.centerCenterLabel.Name = "centerCenterLabel";
+            this.centerCenterLabel.Size = new System.Drawing.Size(0, 55);
+            this.centerCenterLabel.TabIndex = 34;
+            // 
+            // centerLeftLabel
+            // 
+            this.centerLeftLabel.AutoSize = true;
+            this.centerLeftLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.centerLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerLeftLabel.Location = new System.Drawing.Point(52, 79);
+            this.centerLeftLabel.Name = "centerLeftLabel";
+            this.centerLeftLabel.Size = new System.Drawing.Size(0, 55);
+            this.centerLeftLabel.TabIndex = 33;
+            // 
+            // topRightLabel
+            // 
+            this.topRightLabel.AutoSize = true;
+            this.topRightLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.topRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topRightLabel.Location = new System.Drawing.Point(181, 17);
+            this.topRightLabel.Name = "topRightLabel";
+            this.topRightLabel.Size = new System.Drawing.Size(0, 55);
+            this.topRightLabel.TabIndex = 32;
+            // 
+            // topCenterLabel
+            // 
+            this.topCenterLabel.AutoSize = true;
+            this.topCenterLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.topCenterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topCenterLabel.Location = new System.Drawing.Point(116, 17);
+            this.topCenterLabel.Name = "topCenterLabel";
+            this.topCenterLabel.Size = new System.Drawing.Size(0, 55);
+            this.topCenterLabel.TabIndex = 31;
+            // 
+            // topLeftLabel
+            // 
+            this.topLeftLabel.AutoSize = true;
+            this.topLeftLabel.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.topLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLeftLabel.Location = new System.Drawing.Point(52, 16);
+            this.topLeftLabel.Name = "topLeftLabel";
+            this.topLeftLabel.Size = new System.Drawing.Size(0, 55);
+            this.topLeftLabel.TabIndex = 21;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(51, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox7.Location = new System.Drawing.Point(177, 138);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox7.TabIndex = 30;
+            this.richTextBox7.Text = "";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox8.Location = new System.Drawing.Point(114, 138);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox8.TabIndex = 29;
+            this.richTextBox8.Text = "";
+            // 
+            // richTextBox9
+            // 
+            this.richTextBox9.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox9.Location = new System.Drawing.Point(51, 138);
+            this.richTextBox9.Name = "richTextBox9";
+            this.richTextBox9.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox9.TabIndex = 28;
+            this.richTextBox9.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Location = new System.Drawing.Point(177, 77);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox4.TabIndex = 27;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox5.Location = new System.Drawing.Point(114, 77);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox5.TabIndex = 26;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox6.Location = new System.Drawing.Point(51, 77);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox6.TabIndex = 25;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Location = new System.Drawing.Point(177, 16);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox3.TabIndex = 24;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(114, 16);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(57, 55);
+            this.richTextBox2.TabIndex = 23;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.newGameBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bottomRightLabel);
+            this.Controls.Add(this.bottomCenterLabel);
+            this.Controls.Add(this.bottomLeftlabel);
+            this.Controls.Add(this.centerRightLabel);
+            this.Controls.Add(this.centerCenterLabel);
+            this.Controls.Add(this.centerLeftLabel);
+            this.Controls.Add(this.topRightLabel);
+            this.Controls.Add(this.topCenterLabel);
+            this.Controls.Add(this.topLeftLabel);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox9);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button newGameBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label bottomRightLabel;
+        private System.Windows.Forms.Label bottomCenterLabel;
+        private System.Windows.Forms.Label bottomLeftlabel;
+        private System.Windows.Forms.Label centerRightLabel;
+        private System.Windows.Forms.Label centerCenterLabel;
+        private System.Windows.Forms.Label centerLeftLabel;
+        private System.Windows.Forms.Label topRightLabel;
+        private System.Windows.Forms.Label topCenterLabel;
+        private System.Windows.Forms.Label topLeftLabel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+        private System.Windows.Forms.RichTextBox richTextBox9;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Programming Project 4/Programming Project 4/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Project_4
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newGameBtn_Click(object sender, EventArgs e)
+        {
+            int COLS = 3;
+            int ROWS = 3;
+            string TL = "";
+            string TC = "";
+            string TR = "";
+            string CL = "";
+            string CC = "";
+            string CR = "";
+            string BL = "";
+            string BC = "";
+            string BR = "";
+            int[,] gameboard = new int[COLS, ROWS];
+            Random rand = new Random();
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < COLS; j++)
+                {
+                    gameboard[i, j] = rand.Next(1); 
+                }
+            }
+            TL = gameboard[0,0].ToString();
+            TC = gameboard[0,1].ToString();
+            TR = gameboard[0,2].ToString();
+            CL = gameboard[1,0].ToString();
+            CC = gameboard[1,1].ToString();
+            CR = gameboard[1,2].ToString();
+            BL = gameboard[2,0].ToString();
+            BC = gameboard[2,1].ToString();
+            BR = gameboard[2,2].ToString();
+            topLeftLabel.Text = TL;
+            topCenterLabel.Text = TC;
+            topRightLabel.Text = TR;
+            centerLeftLabel.Text = CL;
+            centerCenterLabel.Text = CC;
+            centerRightLabel.Text = CR;
+            bottomLeftlabel.Text = BL;
+            bottomCenterLabel.Text = BC;
+            bottomRightLabel.Text = BR;
+
+
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private string CheckValue(int[,] boardLocation)
+        {
+            string O = "O";
+            string X = "X";
+            string value = boardLocation.ToString();
+            if(value == "0")
+            {
+                return O;
+            }
+            return X;
+        }
+    }
+}
